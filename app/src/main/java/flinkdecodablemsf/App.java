157e9d4e8bf8c3110617eb4a8ae56d7d062343e2
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-24 03:42:07 -0500

    feat: Update App class to use Kinesis for sending sensor records

--- app/src/main/java/flinkdecodablemsf/App.java
+++ app/src/main/java/flinkdecodablemsf/App.java
@@ -3,20 +3,25 @@ package flinkdecodablemsf;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import flinkdecodablemsf.amazon.Kinesis;
 import flinkdecodablemsf.model.Device;
 import flinkdecodablemsf.sources.DeviceThread;
 import flinkdecodablemsf.utils.Config;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
         Config config = new Config();
-        ExecutorService threadPoolService = Executors.newFixedThreadPool(10);
+        AwsCredentialsProvider credentialsProvider = ProfileCredentialsProvider.create("flink");
+        Kinesis kinesis = new Kinesis(credentialsProvider, config.getAwsRegion());
 
+        ExecutorService threadPoolService = Executors.newFixedThreadPool(config.getDeviceCount() * 2);
         while (true) {
             int randomIndex = (int) (Math.random() * config.getDeviceCount());
             Device device = config.getDevices().get(randomIndex);
-            threadPoolService.execute(new DeviceThread(device, config.getInterval()));
+            threadPoolService.execute(new DeviceThread(config.getInterval(), device.getStreamName(), device, kinesis));
         }
     }
 }
