65ba2af33e52f667f7e19ad227935f848820bb4c
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-24 02:42:00 -0500

    feat: Update Config class to use AWS SDK for region configuration

--- app/src/main/java/flinkdecodablemsf/utils/Config.java
+++ app/src/main/java/flinkdecodablemsf/utils/Config.java
@@ -1,24 +1,22 @@
 package flinkdecodablemsf.utils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import flinkdecodablemsf.model.Device;
+import software.amazon.awssdk.regions.Region;
 
 public class Config {
 
     private final List<Device> devices = new ArrayList<>();
     private final long interval = 250L;
     private final String output = "output_stream";
-    private final Map<String, Object> aws = new HashMap<>();
+    private final Region awsRegion = Region.US_EAST_2;
 
     public Config() {
         devices.add(new Device("temp_sensor_001", "temperature", "temp_stream"));
         devices.add(new Device("humidity_sensor_001", "humidity", "humidity_stream"));
         devices.add(new Device("energy_sensor_001", "energy", "energy_stream"));
-        aws.put("region", "us-east-2");
     }
 
     public List<Device> getDevices() {
@@ -37,7 +35,7 @@ public class Config {
         return output;
     }
 
-    public Map<String, Object> getAws() {
-        return aws;
+    public Region getAwsRegion() {
+        return awsRegion;
     }
 }
