dcbfce5486f4c334e85305a65103668499bf484e
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-08-24 03:16:32 -0500

    feat: Add method to send sensor record to Kinesis stream

--- app/src/main/java/flinkdecodablemsf/amazon/Kinesis.java
+++ app/src/main/java/flinkdecodablemsf/amazon/Kinesis.java
@@ -1,23 +1,34 @@
 package flinkdecodablemsf.amazon;
 
+import flinkdecodablemsf.model.SensorRecord;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesis.KinesisAsyncClient;
+import software.amazon.awssdk.services.kinesis.model.PutRecordRequest;
+import software.amazon.awssdk.services.kinesis.model.PutRecordResponse;
 
 public class Kinesis {
 
-    private final AwsCredentialsProvider credentialsProvider;
-    private final Region region;
+    private final KinesisAsyncClient kinesisAsyncClient;
 
     public Kinesis(AwsCredentialsProvider credentialsProvider, Region region) {
-        this.credentialsProvider = credentialsProvider;
-        this.region = region;
-    }
-
-    public KinesisAsyncClient getKinesisAsyncClient() {
-        return KinesisAsyncClient.builder()
+        this.kinesisAsyncClient = KinesisAsyncClient.builder()
                 .region(region)
                 .credentialsProvider(credentialsProvider)
                 .build();
+    }
+
+    public KinesisAsyncClient getKinesisAsyncClient() {
+        return kinesisAsyncClient;
+    }
+
+    public PutRecordResponse sendSensorRecord(String streamName, SensorRecord sensorRecord) {
+        PutRecordRequest request = PutRecordRequest.builder()
+                .streamName(streamName)
+                .partitionKey(sensorRecord.getDeviceId())
+                .data(SdkBytes.fromUtf8String(sensorRecord.getJsonString()))
+                .build();
+        return kinesisAsyncClient.putRecord(request).join();
     }
 }
